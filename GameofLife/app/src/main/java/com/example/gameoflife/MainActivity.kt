5caa39596a6b4d0f90736f9b18f35cca2a2474ac
Aimzj <aimeejhandley@gmail.com>
2019-04-10 10:19:52 +0200

    Dumping android project

---
+++ GameofLife/app/src/main/java/com/example/gameoflife/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.gameoflife
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    var adapter: LifeAdapter? = null
+    var cellsList = ArrayList<Cell>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //load
+        for(i in 1..60)
+        {
+            val randomValue = List(1) { Random.nextInt(0, 1) }
+            println(randomValue)
+            if(randomValue[0] == 1)
+                cellsList.add(Cell(true))
+            else
+                cellsList.add(Cell(true))
+        }
+
+        adapter = LifeAdapter(this, cellsList)
+
+        gridview.adapter = adapter
+    }
+}
