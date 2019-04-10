747007abdb40a17e146963d6afed8489fa6c9d59
David Welman <>
2019-04-10 07:43:07 +0200

    Rewrote map class to make game pure functional, added a main for testing and completed unit tests to test for underpopulation and sustained population. Wrote half game logic in applyRules function

--- GameLogic/src/kgol/GameOfLife.kt
+++ GameLogic/src/kgol/GameOfLife.kt
@@ -1,15 +1,79 @@
 package kgol
 
-class GameMap(val xSize: Int, val ySize: Int) {
+import kotlin.math.max
+import kotlin.math.min
+
+fun main(args: Array<String>) {
+    val gameMap = createEmptyMap(10, 10)
+    //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+
+    //This cluster should live forever in theory
+    gameMap[0][0] = true
+    gameMap[0][1] = true
+    gameMap[1][0] = true
+    gameMap[1][1] = true
+
+    gameMap[5][5] = true
+    gameMap[5][6] = true
+    gameMap[5][7] = true
+    gameMap[6][5] = true
+
+    var newMap = applyRules(gameMap)
+    newMap = applyRules(newMap)
+}
+
+fun createEmptyMap(xSize: Int, ySize: Int) : Array<Array<Boolean>> {
     var cells = arrayOf<Array<Boolean>>()
 
-    init {
-        for (y in 0 until ySize) {
-            var row = arrayOf<Boolean>()
-            for (x in 0 until xSize) {
-                row += false
+    for (y in 0 until ySize) {
+        var row = arrayOf<Boolean>()
+        for (x in 0 until xSize) {
+            row += false
+        }
+        cells += row
+    }
+    return cells
+}
+
+//Conway's Game of Life has 4 rules, all implemented as a pure function below:
+//1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//2. Any live cell with two or three live neighbours lives on to the next generation.
+//3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+//4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+fun applyRules(map: Array<Array<Boolean>>) :  Array<Array<Boolean>> {
+    val xSize: Int = map[0].size
+    val ySize: Int = map.size
+    val returnMap = createEmptyMap(xSize, ySize)
+    var aliveNeighbourCount: Int
+    var neighbours: Int
+
+    //Iterate and perform
+    for (y in 0 until ySize) {
+        for (x in 0 until xSize) {
+            //Current cell
+            aliveNeighbourCount = 0
+
+            //Get neighbours
+            for (j in max(0, y-1)..min(y+1, ySize - 1)) {
+                for (i in max(0, x-1)..min(x+1, xSize - 1)) {
+                    if (i != x || j != y) {
+                        //This is a neighbour
+                        if (map[j][i] == true) {
+                            aliveNeighbourCount++
+                        }
+                    }
+                }
             }
-            cells += row
+
+            returnMap[y][x] = when {
+                aliveNeighbourCount  < 2 && map[y][x] -> false
+                aliveNeighbourCount in 2..3 && map[y][x] -> true
+                else -> false
+            }
+
         }
     }
+
+    return returnMap
 }
--- GameLogic/src/kgol/GameOfLifeTests.kt
+++ GameLogic/src/kgol/GameOfLifeTests.kt
@@ -7,11 +7,111 @@ import org.hamcrest.CoreMatchers.*
 class GameOfLifeTests {
     @Test
     fun createGameMap() {
-        var gameMap = GameMap(50, 50)
-        assertThat(gameMap.xSize, `is`(50))
-        assertThat(gameMap.ySize, `is`(50))
-        assertThat(gameMap.cells[0][0], `is`(false))
-        assertThat(gameMap.cells[24][13], `is`(false))
-        assertThat(gameMap.cells[49][49], `is`(false))
+        val gameMap = createEmptyMap(50, 50)
+        assertThat(gameMap.size, `is`(50))
+        assertThat(gameMap[0].size, `is`(50))
+        assertThat(gameMap[0][0], `is`(false))
+        assertThat(gameMap[24][13], `is`(false))
+        assertThat(gameMap[49][49], `is`(false))
+    }
+
+    @Test
+    fun testUnderpopulationRule() {
+        val gameMap = createEmptyMap(10, 10)
+        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+
+        //This cluster should live forever in theory
+        gameMap[0][0] = true
+        gameMap[0][1] = true
+        gameMap[1][0] = true
+        gameMap[1][1] = true
+
+        gameMap[5][5] = true
+        gameMap[5][6] = true
+        gameMap[5][7] = true
+        gameMap[6][5] = true
+
+        gameMap[7][7] = true
+        gameMap[8][8] = true
+
+        val newMap = applyRules(gameMap)
+        //Check that the old map has not been modified
+        println("Check original map")
+        assertThat(gameMap[0][0], `is`(true))
+        assertThat(gameMap[0][1], `is`(true))
+        assertThat(gameMap[1][0], `is`(true))
+        assertThat(gameMap[1][1], `is`(true))
+
+        assertThat(gameMap[5][5], `is`(true))
+        assertThat(gameMap[5][6], `is`(true))
+        assertThat(gameMap[5][7], `is`(true))
+        assertThat(gameMap[6][5], `is`(true))
+
+        assertThat(gameMap[7][7], `is`(true))
+        assertThat(gameMap[8][8], `is`(true))
+        println("Original map passed")
+
+        //Check that underpopulation rule has been applied
+        println("Check new map")
+        assertThat(newMap[0][0], `is`(true))
+        assertThat(newMap[0][1], `is`(true))
+        assertThat(newMap[1][0], `is`(true))
+        assertThat(newMap[1][1], `is`(true))
+        println("First cluster still living")
+
+        assertThat(newMap[5][5], `is`(true))
+        assertThat(newMap[5][6], `is`(true))
+        assertThat(newMap[5][7], `is`(false))
+        assertThat(newMap[6][5], `is`(true))
+        println("Second cluster has expected death")
+
+        assertThat(newMap[7][7], `is`(false))
+        assertThat(newMap[8][8], `is`(false))
+        println("Third cluster has died completely")
+    }
+
+    @Test
+    fun testLivesOnRule() {
+        val gameMap = createEmptyMap(10, 10)
+        //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+
+        //This cluster should live forever in theory
+        gameMap[0][0] = true
+        gameMap[0][1] = true
+        gameMap[1][0] = true
+        gameMap[1][1] = true
+
+        gameMap[5][5] = true
+        gameMap[5][6] = true
+        gameMap[5][7] = true
+        gameMap[6][5] = true
+
+        var newMap = applyRules(gameMap)
+        //Check that the old map has not been modified
+        println("Check original map")
+        assertThat(gameMap[0][0], `is`(true))
+        assertThat(gameMap[0][1], `is`(true))
+        assertThat(gameMap[1][0], `is`(true))
+        assertThat(gameMap[1][1], `is`(true))
+
+        assertThat(gameMap[5][5], `is`(true))
+        assertThat(gameMap[5][6], `is`(true))
+        assertThat(gameMap[5][7], `is`(true))
+        assertThat(gameMap[6][5], `is`(true))
+        println("Original map passed")
+
+        println("Check that cells survive for 10 generations")
+
+        for (i in 1..10) {
+            var newMap = applyRules(newMap)
+            assertThat(newMap[0][0], `is`(true))
+            assertThat(newMap[0][1], `is`(true))
+            assertThat(newMap[1][0], `is`(true))
+            assertThat(newMap[1][1], `is`(true))
+
+            assertThat(newMap[5][5], `is`(true))
+            assertThat(newMap[5][6], `is`(true))
+            assertThat(newMap[6][5], `is`(true))
+        }
     }
 }
